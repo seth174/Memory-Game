a1192a6a859d2a461228d12fedec93356491d65b
seth174 <50536444+seth174@users.noreply.github.com>
2021-01-11 01:09:27 -0500

    Added the ability to change between shapes

--- GUI.py
+++ GUI.py
@@ -1,12 +1,43 @@
 import pygame
 
-def draw_shape(shape, position):
-    pass
+def draw_shape(screen, shape, column_row):
+    x_position, y_position = column_row
+    x_position = x_position * 200
+    y_position = y_position * 200
+    if shape == 1:
+        pygame.draw.rect(screen, (0, 255, 0), (x_position + 25, y_position + 25, 150, 150))
+    elif shape == 2:
+        pygame.draw.polygon(screen, (0, 0, 255), [(x_position + 25, y_position + 175),
+                                                  (x_position + 100, y_position + 25),
+                                                  (x_position + 175, y_position + 175)])
+    elif shape == 3:
+        pygame.draw.circle(screen, (255, 0, 0), (x_position + 100, y_position + 100), 75)
+    elif shape == 4:
+        pygame.draw.ellipse(screen, (255, 0, 255), (x_position + 30, y_position + 15, 125, 175))
+    pygame.display.update()
+
+def initialize_board(screen):
+    for i in range(0, 601, 200):
+        pygame.draw.line(screen, (0, 0, 0), (i, 0), (i, 600))
+        pygame.draw.line(screen, (0, 0, 0), (0, i), (600, i))
+    pygame.display.update()
 
 def get_shape(shape_name):
     if shape_name == 'rectangle':
         return 50, 50, 50, 50
 
-
 def get_color():
     return 255, 0, 0
+
+def get_column_row(x_position, y_position):
+    return x_position // 200, y_position // 200
+
+def fill_rectangle(screen, column_row):
+    x, y = column_row
+    x = x * 200
+    y = y * 200
+    screen.fill((100, 100, 100), (x + 1, y + 1, 198, 198))
+    pygame.display.update()
+
+
+
--- Main.py
+++ Main.py
@@ -12,14 +12,25 @@ screen.fill((100, 100, 100))
 pygame.display.set_caption('Memory Game')
 pygame.display.update()
 running = True
+GUI.initialize_board(screen)
+list_tracker = {}
+for i in range(0, 3):
+    for j in range(0, 3):
+        list_tracker[(i, j)] = 1
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            print(pygame.mouse.get_pos())
+            x, y = pygame.mouse.get_pos()
+            column, row = GUI.get_column_row(x, y)
+            tracker = list_tracker[(row, column)]
+            if tracker == 5:
+                tracker = 1
+            GUI.fill_rectangle(screen, (column, row))
+            GUI.draw_shape(screen, tracker, (column, row))
+            list_tracker[(row, column)] = tracker + 1
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_s:
                 pygame.draw.rect(screen, (255, 0, 0), (50, 50, 50, 50))
                 pygame.display.update()
-
